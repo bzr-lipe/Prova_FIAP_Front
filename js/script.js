e35568fbb825ce41376538c9fe856eed6d540ace
bzr-lipe <bzr.lipe@gmail.com>
2023-01-22 04:49:40 -0300

    accordion 1 fixes, accordion 2 on progress

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ const accordion1 = document.getElementById('accordion-1');
 const accordion2 = document.getElementById('accordion-2');
 const accordionBtnElement1 = document.getElementById('btn-element-action1');
 const accordionBtnElement2 = document.getElementById('btn-element-action2');
+const accordionTopContent1 = document.getElementById('accordion-top-elements1');
+const accordionTopContent2 = document.getElementById('accordion-top-elements2');
 
 
-btnGaveta1.onclick = function(){
+accordionTopContent1.onclick = function(){
     if(accordion1.style.height == '') {
         accordion1.setAttribute('style', 'height: 382px');
         accordionBtnElement1.setAttribute('style', 'transform: rotate(0deg)');
@@ -18,13 +20,15 @@ btnGaveta1.onclick = function(){
 };
 
 
-btnGaveta2.onclick = function(){
+accordionTopContent2.onclick = function(){
     if(accordion2.style.height == '') {
-        accordion2.setAttribute('style', 'height: 432px');
+        accordion2.setAttribute('style', 'height: 506px');
         accordionBtnElement2.setAttribute('style', 'transform: rotate(0deg)');
     }else {
-        accordion2.removeAttribute('style', 'height: 432px');
+        accordion2.removeAttribute('style', 'height: 506px');
         accordionBtnElement2.removeAttribute('style', 'transform: rotate(0deg)');
     }
     
 };
+
+
