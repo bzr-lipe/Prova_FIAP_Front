3d0e98a41e84a2aacd03c1d527a9cbf1eb23fd45
bzr-lipe <bzr.lipe@gmail.com>
2023-01-22 06:34:09 -0300

    fixing turmas, accordion desktop ok

--- js/script.js
+++ js/script.js
@@ -6,15 +6,18 @@ const accordionBtnElement1 = document.getElementById('btn-element-action1');
 const accordionBtnElement2 = document.getElementById('btn-element-action2');
 const accordionTopContent1 = document.getElementById('accordion-top-elements1');
 const accordionTopContent2 = document.getElementById('accordion-top-elements2');
+const accordionItem = document.getElementsByClassName('accordion2-item-content');
 
 
 accordionTopContent1.onclick = function(){
     if(accordion1.style.height == '') {
         accordion1.setAttribute('style', 'height: 382px');
+        accordionTopContent1.setAttribute('style', 'color: var(--magenta)');
         accordionBtnElement1.setAttribute('style', 'transform: rotate(0deg)');
     }else {
-        accordion1.removeAttribute('style', 'height: 382px');
+        accordion1.removeAttribute('style', 'height: 382px!important');
         accordionBtnElement1.removeAttribute('style', 'transform: rotate(0deg)');
+        accordionTopContent1.removeAttribute('style', 'color: var(--magenta)')
     }
     
 };
@@ -22,11 +25,13 @@ accordionTopContent1.onclick = function(){
 
 accordionTopContent2.onclick = function(){
     if(accordion2.style.height == '') {
-        accordion2.setAttribute('style', 'height: 506px');
+        accordion2.setAttribute('style', 'height: 519px');
+        accordionTopContent2.setAttribute('style', 'color: var(--magenta)');
         accordionBtnElement2.setAttribute('style', 'transform: rotate(0deg)');
     }else {
-        accordion2.removeAttribute('style', 'height: 506px');
+        accordion2.removeAttribute('style', 'height: 519px');
         accordionBtnElement2.removeAttribute('style', 'transform: rotate(0deg)');
+        accordionTopContent2.removeAttribute('style', 'color: var(--magenta)');
     }
     
 };
