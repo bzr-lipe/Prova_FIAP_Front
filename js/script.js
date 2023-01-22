5f3b27d8ec754704aba9a8131c15880235633f7f
bzr-lipe <bzr.lipe@gmail.com>
2023-01-22 03:25:28 -0300

    accordion 1 - almost done, no bugs, only desktop view

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
-const btnGaveta = document.getElementById('btn-accordion1');
+const btnGaveta1 = document.getElementById('btn-accordion1');
+const btnGaveta2 = document.getElementById('btn-accordion2');
 const accordion1 = document.getElementById('accordion-1');
+const accordion2 = document.getElementById('accordion-2');
+const accordionBtnElement1 = document.getElementById('btn-element-action1');
+const accordionBtnElement2 = document.getElementById('btn-element-action2');
 
-btnGaveta.onclick = function(){
-    
-    if(accordion1.style.height=="90px"){
-        alert('oi')
+
+btnGaveta1.onclick = function(){
+    if(accordion1.style.height == '') {
+        accordion1.setAttribute('style', 'height: 382px');
+        accordionBtnElement1.setAttribute('style', 'transform: rotate(0deg)');
+    }else {
+        accordion1.removeAttribute('style', 'height: 382px');
+        accordionBtnElement1.removeAttribute('style', 'transform: rotate(0deg)');
     }
+    
+};
+
+
+btnGaveta2.onclick = function(){
+    if(accordion2.style.height == '') {
+        accordion2.setAttribute('style', 'height: 432px');
+        accordionBtnElement2.setAttribute('style', 'transform: rotate(0deg)');
+    }else {
+        accordion2.removeAttribute('style', 'height: 432px');
+        accordionBtnElement2.removeAttribute('style', 'transform: rotate(0deg)');
+    }
+    
 };
