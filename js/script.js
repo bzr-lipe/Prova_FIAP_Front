f90836be420f3122e32790b74e9bea2386a555b0
bzr-lipe <bzr.lipe@gmail.com>
2023-01-22 23:16:56 -0300

    desktop view ok, fixing media queries

--- js/script.js
+++ js/script.js
@@ -8,28 +8,30 @@ const accordionTopContent1 = document.getElementById('accordion-top-elements1');
 const accordionTopContent2 = document.getElementById('accordion-top-elements2');
 const accordionItem = document.querySelectorAll('.accordion2-item-content');
 const scrollDownText = document.getElementById('scroll-text');
-const arrowDownSymbol = document.getElementById('arrow-symbol')
+const arrowDownSymbol = document.getElementById('arrow-symbol');
+const text1 = document.getElementById('list-text1');
+const text2 = document.getElementById('list-text2');
 
-accordionTopContent1.onclick = function(){
-    if(accordion1.style.height == '') {
+accordionTopContent1.onclick = function () {
+    if (accordion1.style.height == '') {
         accordion1.setAttribute('style', 'height: 382px');
         accordionTopContent1.setAttribute('style', 'color: var(--magenta)');
         accordionBtnElement1.setAttribute('style', 'transform: rotate(0deg)');
-    }else {
+    } else {
         accordion1.removeAttribute('style', 'height: 382px!important');
         accordionBtnElement1.removeAttribute('style', 'transform: rotate(0deg)');
         accordionTopContent1.removeAttribute('style', 'color: var(--magenta)')
     }
-    
+
 };
 
 
-accordionTopContent2.onclick = function(){
-    if(accordion2.style.height == '') {
+accordionTopContent2.onclick = function () {
+    if (accordion2.style.height == '') {
         accordion2.setAttribute('style', 'height: 519px');
         accordionTopContent2.setAttribute('style', 'color: var(--magenta)');
         accordionBtnElement2.setAttribute('style', 'transform: rotate(0deg)');
-    }else {
+    } else {
         accordion2.removeAttribute('style', 'height: 519px');
         accordionBtnElement2.removeAttribute('style', 'transform: rotate(0deg)');
         accordionTopContent2.removeAttribute('style', 'color: var(--magenta)');
@@ -39,19 +41,29 @@ accordionTopContent2.onclick = function(){
 window.addEventListener('scroll', () => {
     const scrolled = window.scrollY;
 
-    if(scrolled>=150){
-        scrollDownText.style.fontSize="0px";
-    }else {
-        scrollDownText.style.fontSize ="9px";
+    if (scrolled >= 150) {
+        scrollDownText.style.fontSize = "0px";
+    } else {
+        scrollDownText.style.fontSize = "9px";
     }
 
-    if(scrolled>=400){
-        arrowDownSymbol.style.opacity="0%";
-    }else {
-        arrowDownSymbol.style.opacity ="100%";
+    if (scrolled >= 400) {
+        arrowDownSymbol.style.opacity = "0%";
+    } else {
+        arrowDownSymbol.style.opacity = "100%";
     }
 })
 
-const text1 = document.getElementById('list-text1').onmouseover = () =>{
-    let marker1 = document.getElementById('list-marker1')
+text1.onmouseover = () => {
+    document.getElementById('list-marker1').classList.add('list-marker-active')
+}
+text2.onmouseover = () => {
+    document.getElementById('list-marker2').classList.add('list-marker-active')
+}
+
+text1.onmouseout = () => {
+    document.getElementById('list-marker1').classList.remove('list-marker-active')
+}
+text2.onmouseout = () => {
+    document.getElementById('list-marker2').classList.remove('list-marker-active')
 }
